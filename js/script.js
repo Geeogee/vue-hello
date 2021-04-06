24678d2ee39ac817d36932698b5fa5829a43c08c
Gioele <gioele.armiento@gmail.com>
2021-04-06 16:12:40 +0200

    Exercise finished (bonus too)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 function init() {
 
+    // TODO TODAY
+    // REPO: vue-hello
+    // GOAL: stampare a schermo il contenuto di una variabile dentro un h1
+    // BONUS: utilizzare un data come nome del file immagine in un tag img
 
+    new Vue ({
+
+        el: "#app",
+        data: {
+
+            "message" : "Rathalos",
+            "rathalos" : "img/MHRise-Rathalos_Icon.png"
+        }
+    
+    })
 }
 
 document.addEventListener("DOMContentLoaded", init);    
