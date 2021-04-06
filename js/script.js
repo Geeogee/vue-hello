e544c3049b91a250b0c4043ded0fa943f4834af5
Gioele <gioele.armiento@gmail.com>
2021-04-06 16:49:06 +0200

    Added a flag

--- js/script.js
+++ js/script.js
@@ -8,22 +8,33 @@ function init() {
     new Vue ({
 
         el: "#app",
+
         data: {
 
             "name" : "Rathalos",
             "monster" : "img/MHRise-Rathalos_Icon.png",
+            "clicked" : false
         },
 
         methods: {
 
             changeMonster: function() {
 
-                this.name = "Rajang";
-                this.monster = "img/rajang.png";   
+                if(this.clicked) {
+                    this.name = "Rathalos";
+                    this.monster = "img/MHRise-Rathalos_Icon.png";
+                    this.clicked = false;
+                } else {
+                    this.name = "Rajang";
+                    this.monster = "img/rajang.png"; 
+                    this.clicked = true;
+                }
+                 
+                
             }
         }
     
-    })
+    });
 }
 
 document.addEventListener("DOMContentLoaded", init);    
